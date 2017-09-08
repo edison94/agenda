bdaae2790c6c7837c538b60e00783641a8120f02
Kombusting <kombusting@gmail.com>
2017-09-08 13:21:46 +0200

    Creado carpetas resources para evitar errores MAVEN

--- src/main/java/spring/dao/DepartamentoDAOImpl.java
+++ src/main/java/spring/dao/DepartamentoDAOImpl.java
@@ -6,16 +6,14 @@ import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
-import org.hibernate.Criteria;
-import org.hibernate.Query;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Transactional;
 
-import spring.model.Categoria;
 import spring.model.Departamento;
 
 @Repository
