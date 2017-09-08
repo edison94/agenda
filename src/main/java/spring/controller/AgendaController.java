91803376db43f9a94d9ee29f46972789702f62f8
Kombusting <kombusting@gmail.com>
2017-09-08 13:55:25 +0200

    Commit

--- src/main/java/spring/controller/AgendaController.java
+++ src/main/java/spring/controller/AgendaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import spring.model.Categoria;
 import spring.service.ICategoriaService;
 import spring.service.IDepartamentoService;
 
