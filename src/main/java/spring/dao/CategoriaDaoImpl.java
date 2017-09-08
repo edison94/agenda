aa3ba08fe7460a4ae7c3390b2f2e3de4cc95083e
LluisOliveras <LluisOliveras@github.com>
2017-09-08 13:48:27 +0200

    categoria dao funcional

--- src/main/java/spring/dao/CategoriaDaoImpl.java
+++ src/main/java/spring/dao/CategoriaDaoImpl.java
@@ -8,12 +8,14 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 import javax.transaction.Transactional;
 
+import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Repository;
+
 
 import spring.model.Categoria;
 
@@ -32,9 +34,6 @@ public class CategoriaDaoImpl implements ICategoriaDao {
 		query.select(root);
 		Query<Categoria> q = session.createQuery(query);
 		List<Categoria> categorias = q.getResultList();
-		for (Categoria c : categorias) {
-			System.out.println(c);
-		}
 		return categorias;
 
 	}
@@ -46,8 +45,8 @@ public class CategoriaDaoImpl implements ICategoriaDao {
 	
 	@Transactional
 	public void delete(int id) {
-		// TODO Auto-generated method stub
-
+		Categoria c= get(id);
+		sessionFactory.getCurrentSession().delete(c);
 	}
 	
 	@Transactional
@@ -56,13 +55,9 @@ public class CategoriaDaoImpl implements ICategoriaDao {
 		CriteriaBuilder builder = session.getCriteriaBuilder();
 		CriteriaQuery<Categoria> query = builder.createQuery(Categoria.class);
 		Root<Categoria> root = query.from(Categoria.class);
-		query.select(root);
+		query.select(root).where(builder.equal(root.get("idcategorias"),id));
 		Query<Categoria> q = session.createQuery(query);
-		List<Categoria> categorias = q.getResultList();
-		for (Categoria c : categorias) {
-			System.out.println(c);
-		}
-		return null;
+		return q.uniqueResult();
 	}
 
 }
--- src/main/java/spring/model/Categoria.java
+++ src/main/java/spring/model/Categoria.java
@@ -77,4 +77,12 @@ public class Categoria implements java.io.Serializable {
 		this.empleadoses = empleadoses;
 	}
 
+	@Override
+	public String toString() {
+		return "Categoria [idcategorias=" + idcategorias + ", nombre=" + nombre + ", descripcion=" + descripcion
+				+ "]";
+	}
+	
+	
+
 }
