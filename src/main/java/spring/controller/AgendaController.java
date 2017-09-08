91cf735ac46b1be9848f2a8da9a6887f9f1ebfcb
pisoni10 <31759405+pisoni10@users.noreply.github.com>
2017-09-08 12:07:28 +0200

    He creado en el Controller el GetCategoria

--- src/main/java/spring/controller/AgendaController.java
+++ src/main/java/spring/controller/AgendaController.java
@@ -1,9 +1,16 @@
 package spring.controller;
 
+import java.util.ArrayList;
+
+import javax.persistence.Column;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import antlr.collections.List;
 
 @Controller
 public class AgendaController {
@@ -13,4 +20,32 @@ public class AgendaController {
 	public ModelAndView getDepartamentos() {
 		return new ModelAndView("home");
 	}
+	//CREAR EN CONTROLLER GETCATEGORIA
+	@RequestMapping("/")
+	public ModelAndView handleRequest() throws Exception {
+		List<Categoria> listUsers = addCategoria.list();//mostrar lista
+		ModelAndView model = new ModelAndView("UserList");//nuevo objeto en la lista
+		model.addObject("userList", listarCategorias);//guardar
+		return model;
+	}
+	
+		
+		
+		
+		
+		
+		
+		//tabla del get categoria
+		@ModelAttribute("categorias")
+		public String initializeCategorias() {
+
+			List categoria = (List) new ArrayList();
+			categoria.add("Programador");
+			categoria.add("Analista");
+			categoria.add("Tecnico de Recursos Humanos");
+			categoria.add("Secretaria");
+			categoria.add("Recepcionista");
+			categoria.add("Master del Universo");
+			return "home";
+}
 }
