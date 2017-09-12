9748df8d17158cb8f22b802d4ccf97ca95998282
Kombusting <kombusting@gmail.com>
2017-09-12 15:57:55 +0200

    errores

--- src/main/java/spring/dao/ICategoriaDao.java
+++ src/main/java/spring/dao/ICategoriaDao.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 import spring.model.Categoria;
 
-public interface ICategoriaDao {
+public interface ICategoriaDAO {
 	public List<Categoria> list();
 	public void saveOrUpdate(Categoria cat);
 	public void delete(int id);
