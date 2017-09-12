064a67d4a7f9cc074de2cb7a17880471e3e73abe
LluisOliveras <LluisOliveras@github.com>
2017-09-12 12:37:54 +0200

    Persona DAO getPersona()

---
+++ src/main/java/spring/dao/IPersonaDAO.java
@@ -0,0 +1,7 @@
+package spring.dao;
+
+import spring.model.Persona;
+
+public interface IPersonaDAO {
+	public Persona getPersona(int id);
+}
---
+++ src/main/java/spring/dao/PersonaDAOImpl.java
@@ -0,0 +1,33 @@
+package spring.dao;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+
+import spring.model.Departamento;
+import spring.model.Persona;
+
+public class PersonaDAOImpl implements IPersonaDAO {
+
+	@Autowired
+	@Qualifier("sessionFactory")
+	private SessionFactory sessionFactory;
+	
+	public Persona getPersona(int id) {
+		Session session = sessionFactory.getCurrentSession();
+		CriteriaBuilder builder = session.getCriteriaBuilder();
+		CriteriaQuery<Persona> query = builder.createQuery(Persona.class);
+		Root<Persona> root = query.from(Persona.class);
+		query.select(root).where(builder.equal(root.get("idpersonas"), id));
+		Query<Persona> q = session.createQuery(query);
+		return q.getSingleResult();
+		
+	}
+
+}
