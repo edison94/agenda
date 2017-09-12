3e0ccb5236f0be348b2046767e03bb5ab6d75e1a
LluisOliveras <LluisOliveras@github.com>
2017-09-12 13:56:12 +0200

    ahora no peta

--- src/main/java/spring/controller/AgendaController.java
+++ src/main/java/spring/controller/AgendaController.java
@@ -26,8 +26,8 @@ public class AgendaController {
 	@Autowired
 	private IDepartamentoService departamentoService;
 	
-	/*@Autowired
-	private IPersonaService personaService;*/
+	@Autowired
+	private IPersonaService personaService;
 	
 	
 	/**************************************************
@@ -99,7 +99,6 @@ public class AgendaController {
 	/**************************************************
 	 * Persona
 	 **************************************************/
-	/*
 	@RequestMapping(value = "/personas", method = RequestMethod.GET)	
 	public String getListadoPersonas() {
 		return "Personas";
@@ -134,7 +133,6 @@ public class AgendaController {
 		personaService.deletePersona(id);
 		return "redirect: /agenda/personas";
 	}
-	*/
 	/**************************************************
 	 * Departamento
 	 **************************************************/
