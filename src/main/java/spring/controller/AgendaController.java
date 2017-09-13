08050afdf7083b71b9c5ee804af414f3baf80c2c
Edison <lrobalino@iesjoandaustria.org>
2017-09-13 17:03:28 +0200

    recuperada la parte de empleado

--- src/main/java/spring/controller/AgendaController.java
+++ src/main/java/spring/controller/AgendaController.java
@@ -32,20 +32,19 @@ import spring.service.ISearchService;
 public class AgendaController {
 	@Autowired
 	private ICategoriaService categoriaService;
-	
+
 	@Autowired
 	private IDepartamentoService departamentoService;
-	
+
 	@Autowired
 	private IPersonaService personaService;
-	
+
 	@Autowired
 	private IEmpleadoService empleadoService;
-	
+
 	@Autowired
 	private ISearchService searchService;
-	
-	
+
 	/**************************************************
 	 * BEANS
 	 **************************************************/
@@ -53,42 +52,42 @@ public class AgendaController {
 	public Categoria getCategoriaObjectNew() {
 		return new Categoria();
 	}
-	
+
 	@ModelAttribute("departamento")
 	public Departamento getDepartamentoObjectNew() {
 		return new Departamento();
 	}
-	
+
 	@ModelAttribute("persona")
 	public Persona getPersonaObjectNew() {
 		return new Persona();
 	}
-	
+
 	@ModelAttribute("empleado")
 	public Empleado getEmpleadoObjectNew() {
 		return new Empleado();
 	}
-	
+
 	@ModelAttribute("categorias")
 	public List<Categoria> getCategorias() {
 		return categoriaService.listarCategorias();
 	}
-	
+
 	@ModelAttribute("departamentos")
 	public List<Departamento> getDepartamentos() {
 		return departamentoService.listarDepartamentos();
 	}
-	
+
 	@ModelAttribute("personas")
-	public List<Persona> getPersonas(){
+	public List<Persona> getPersonas() {
 		return personaService.listarPersonas();
 	}
-	
+
 	@ModelAttribute("empleados")
-	public List<Empleado> getEmpleados(){
+	public List<Empleado> getEmpleados() {
 		return empleadoService.listarEmpleados();
 	}
-	
+
 	/**************************************************
 	 * HOME
 	 **************************************************/
@@ -98,11 +97,11 @@ public class AgendaController {
 		searchService.searchPersonasByNombre("hola");
 		return new ModelAndView("home");
 	}
-	
+
 	@RequestMapping(value = "/opciones", method = RequestMethod.GET)
 	public @ResponseBody List<String> options(@RequestParam("sujeto") String sujeto) {
 		ArrayList<String> opciones = new ArrayList<String>();
-		if(sujeto.equals("empleado")){
+		if (sujeto.equals("empleado")) {
 			opciones.add("Categoría");
 			opciones.add("Departamento");
 		}
@@ -111,17 +110,17 @@ public class AgendaController {
 		opciones.add("Teléfono");
 		return opciones;
 	}
-	
+
 	/**************************************************
 	 * Categoria
 	 **************************************************/
-	
-	@RequestMapping(value = "/categorias", method = RequestMethod.GET)	
+
+	@RequestMapping(value = "/categorias", method = RequestMethod.GET)
 	public String getListadoCategorias() {
 		return "categorias";
 	}
-	
-	@RequestMapping(value = {"/categorias/add","/categorias/edit"} ,method = RequestMethod.POST)
+
+	@RequestMapping(value = { "/categorias/add", "/categorias/edit" }, method = RequestMethod.POST)
 	public String saveCategoria(@Valid Categoria categoria, BindingResult result) {
 		if (result.hasErrors()) {
 			return "formCategoria";
@@ -129,77 +128,69 @@ public class AgendaController {
 		categoriaService.saveOrUpdate(categoria);
 		return "redirect: /agenda/categorias";
 	}
-	
-	@RequestMapping(value = "/categorias/add", method = RequestMethod.GET)		
+
+	@RequestMapping(value = "/categorias/add", method = RequestMethod.GET)
 	public String formCategoria() {
 		return "formCategoria";
 	}
-	
-	@RequestMapping(value = "/categorias/edit", method = RequestMethod.GET)		
-	public String editCategoria(@RequestParam("id")int id, ModelMap map) {		
-		map.addAttribute("categoria",categoriaService.getCategoria(id));
+
+	@RequestMapping(value = "/categorias/edit", method = RequestMethod.GET)
+	public String editCategoria(@RequestParam("id") int id, ModelMap map) {
+		map.addAttribute("categoria", categoriaService.getCategoria(id));
 		return "formCategoria";
 	}
-	
-	@RequestMapping(value = "/categorias/delete", method = RequestMethod.GET)		
+
+	@RequestMapping(value = "/categorias/delete", method = RequestMethod.GET)
 	public String deleteCategoria(@RequestParam int id) {
 		categoriaService.deleteCategoria(id);
 		return "redirect: /agenda/categorias";
 	}
-	
+
 	/**************************************************
 	 * Persona
 	 **************************************************/
-	
-	/*@RequestMapping(value = "/personas", method = RequestMethod.GET)	
-	public String getListadoPersonas() {
-		return "personas";
-	}
-	
-	
-	
-	@RequestMapping(value = {"/personas/add","/personas/edit"} ,method = RequestMethod.POST)
-	public String savePersona(@Valid Persona persona, BindingResult result) {
-		if (result.hasErrors()) {
-			return "formPersona";
-		}
-		personaService.saveOrUpdate(persona);
-		return "redirect: /agenda/personas";
-	}
-	
-	
-	@RequestMapping(value = "/personas/add", method = RequestMethod.GET)		
-	public String formPersonas() {
-		return "formPersona";
-	}
-	
-	@RequestMapping(value = "/personas/edit", method = RequestMethod.GET)		
-	public String editPersona(@RequestParam("id")int id, ModelMap map) {		
-		map.addAttribute("persona",personaService.getPersona(id));
-		return "formPersona";
-	}
-	
-	
-	@RequestMapping(value = "/personas/delete", method = RequestMethod.GET)		
-	public String deletePersona(@RequestParam int id) {
-		personaService.deletePersona(id);
-		return "redirect: /agenda/personas";
-	}*/
+
+	/*
+	 * @RequestMapping(value = "/personas", method = RequestMethod.GET) public
+	 * String getListadoPersonas() { return "personas"; }
+	 * 
+	 * 
+	 * 
+	 * @RequestMapping(value = {"/personas/add","/personas/edit"} ,method =
+	 * RequestMethod.POST) public String savePersona(@Valid Persona persona,
+	 * BindingResult result) { if (result.hasErrors()) { return "formPersona"; }
+	 * personaService.saveOrUpdate(persona); return
+	 * "redirect: /agenda/personas"; }
+	 * 
+	 * 
+	 * @RequestMapping(value = "/personas/add", method = RequestMethod.GET)
+	 * public String formPersonas() { return "formPersona"; }
+	 * 
+	 * @RequestMapping(value = "/personas/edit", method = RequestMethod.GET)
+	 * public String editPersona(@RequestParam("id")int id, ModelMap map) {
+	 * map.addAttribute("persona",personaService.getPersona(id)); return
+	 * "formPersona"; }
+	 * 
+	 * 
+	 * @RequestMapping(value = "/personas/delete", method = RequestMethod.GET)
+	 * public String deletePersona(@RequestParam int id) {
+	 * personaService.deletePersona(id); return "redirect: /agenda/personas"; }
+	 */
 
 	/**************************************************
 	 * Departamento
 	 **************************************************/
-	@RequestMapping(value = "/departamentos", method = RequestMethod.GET)	
+	@RequestMapping(value = "/departamentos", method = RequestMethod.GET)
 	public String getListadoDepartamentos() {
 		return "departamentos";
 	}
-	
-	@RequestMapping(value = "/departamentos/add", method = RequestMethod.GET)		
+
+	@RequestMapping(value = "/departamentos/add", method = RequestMethod.GET)
 	public String formDepartamento() {
 		return "formDepartamento";
 	}
-	
-	@RequestMapping(value = {"/departamentos/add","/departamentos/edit"} ,method = RequestMethod.POST)
+
+	@RequestMapping(value = { "/departamentos/add", "/departamentos/edit" }, method = RequestMethod.POST)
 	public String saveDepartamento(@Valid Departamento departamento, BindingResult result) {
 		if (result.hasErrors()) {
 			return "formDepartamento";
@@ -207,36 +198,60 @@ public class AgendaController {
 		departamentoService.saveOrUpdate(departamento);
 		return "redirect: /agenda/departamentos";
 	}
-	
-	@RequestMapping(value = "/departamentos/edit", method = RequestMethod.GET)		
-	public String editDepartamento(@RequestParam("id")int id, ModelMap map) {		
-		map.addAttribute("departamento",departamentoService.get(id));
+
+	@RequestMapping(value = "/departamentos/edit", method = RequestMethod.GET)
+	public String editDepartamento(@RequestParam("id") int id, ModelMap map) {
+		map.addAttribute("departamento", departamentoService.get(id));
 		return "formDepartamento";
 	}
-	
+
 	@RequestMapping(value = "/departamentos/delete", method = RequestMethod.GET)
-	public String deleteDepartamento(@RequestParam("id")int id) throws AgendaException {
+	public String deleteDepartamento(@RequestParam("id") int id) throws AgendaException {
 		try {
 			departamentoService.delete(id);
 		} catch (Exception e) {
-			throw new AgendaException("redirect: /agenda/departamentos","No se puede borrar");
+			throw new AgendaException("redirect: /agenda/departamentos", "No se puede borrar");
 		}
 		return "redirect: /agenda/departamentos";
 	}
 
-	
-	@ResponseStatus(value=HttpStatus.CONFLICT,reason="Data integrity violation") 
+	@ResponseStatus(value = HttpStatus.CONFLICT, reason = "Data integrity violation")
 	@ExceptionHandler(AgendaException.class)
 	public void handleCustomException() {
-		//esto se haria mejor con ajax
+		// esto se haria mejor con ajax
 	}
 
-	
 	/**************************************************
 	 * Empleado
 	 **************************************************/
-	@RequestMapping(value = "/empleados", method = RequestMethod.GET)	
+	@RequestMapping(value = "/empleados", method = RequestMethod.GET)
 	public String getListadoEmpleados() {
 		return "empleados";
+	}
+
+	@RequestMapping(value = "/empleados/add", method = RequestMethod.GET)
+	public String formEmpleado() {
+		return "formEmpleado";
+	}
+
+	@RequestMapping(value = "/empleados/edit", method = RequestMethod.GET)
+	public String editEmpleado(@RequestParam("id") int id, ModelMap map) {
+		map.addAttribute("empleado", empleadoService.get(id));
+		return "formEmpleado";
+	}
+
+	@RequestMapping(value = { "/empleados/add", "/empleados/edit" }, method = RequestMethod.POST)
+	public String saveEmpleado(@Valid Empleado empleado, BindingResult result) {
+		if (result.hasErrors()) {
+			return "formEmpleado";
+		}
+		empleadoService.saveOrUpdate(empleado);
+		return "redirect: /agenda/empleados";
+	}
+
+	@RequestMapping(value = "/empleados/delete", method = RequestMethod.GET)
+	public String deleteEmpleado(@RequestParam("id") int id) {
+		empleadoService.delete(id);
+		return "redirect: /agenda/empleados";
 	}
 }
