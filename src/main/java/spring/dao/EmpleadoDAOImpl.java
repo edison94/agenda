02373a7c6685d71f45c8ca744e005b410ce5e3ea
Kombusting <kombusting@gmail.com>
2017-09-13 16:53:21 +0200

    c

--- src/main/java/spring/dao/EmpleadoDAOImpl.java
+++ src/main/java/spring/dao/EmpleadoDAOImpl.java
@@ -13,7 +13,6 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import spring.model.Departamento;
 import spring.model.Empleado;
 
 @Repository
